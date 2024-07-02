3c242d75e12acbe46d00bf4e11328eba58c1e647
peshelg <peshelg@afrosoft.co.zw>
2024-07-02 17:32:02 +0200

    added view pop

--- app/case/page.jsx
+++ app/case/page.jsx
@@ -1,3 +1,13 @@
+export default function CasePage() {
+    return (
+        <div>
+            <h1>Case Page</h1>
+            </div>
+    )
+    
+}
+
+
 // "use client";
 // import { useState, useEffect, Suspense } from "react";
 // import pb from "@/lib/connection";
