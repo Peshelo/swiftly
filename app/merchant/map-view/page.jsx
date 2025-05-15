ee1570a453741b9c4e62af0201cbfbfb544067d7
Peshel <gomopeshelo@gmail.com>
2025-05-16 00:07:30 +0200

    added map icons

--- app/merchant/map-view/page.jsx
+++ app/merchant/map-view/page.jsx
@@ -19,17 +19,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { FaDirections } from "react-icons/fa";
 import { set } from "date-fns";
-  const siren = new Audio("/assets/audio/siren.mp3");
+const siren = new Audio("/assets/audio/siren.mp3");
 
 // import MapboxDirections from "@mapbox/mapbox-gl-directions";
 function MapPage() {
   const [cases, setCases] = useState([]);
+  const [selectedCase, setSelectedCase] = useState(null);
+
   const mapBoxKey = process.env.NEXT_PUBLIC_MAPBOX_KEY;
   const [coordinates, setCoordinates] = useState({
     latitude: -17.825165,
     longitude: 31.053028,
   });
-
 
   const fetchCases = async () => {
     try {
@@ -44,32 +45,31 @@ function MapPage() {
   useEffect(() => {
     fetchCases();
     pb.collection("cases").subscribe("*", (e) => {
-        if (e.action === "create") {
-
-            setCases((prevCases) => [...prevCases, e.record]);
-            setCoordinates({
-                latitude: e.record.latitude,
-                longitude: e.record.longitude,
-            });
-            console.log(e.record);
-            if (e.record?.title.toString().toLowerCase().includes('sos')) {
-                // Play the siren sound
-                siren.play();
-            }
-            siren.play();
-        } else if (e.action === "update") {
-            setCases((prevCases) =>
-            prevCases.map((mycase) =>
-                mycase.id === e.record.id ? { ...mycase, ...e.record } : mycase
-            )
-            );
-        } else if (e.action === "delete") {
-            setCases((prevCases) =>
-            prevCases.filter((mycase) => mycase.id !== e.record.id)
-            );
-        }
+      if (e.action === "create") {
+        setCases((prevCases) => [...prevCases, e.record]);
+        setCoordinates({
+          latitude: e.record.latitude,
+          longitude: e.record.longitude,
         });
-     
+        toast.success(`New case created: ${e.record.title}`);
+        console.log(e.record);
+        if (e.record?.title.toString().toLowerCase().includes("sos")) {
+          // Play the siren sound
+          siren.play();
+        }
+        // siren.play();
+      } else if (e.action === "update") {
+        setCases((prevCases) =>
+          prevCases.map((mycase) =>
+            mycase.id === e.record.id ? { ...mycase, ...e.record } : mycase
+          )
+        );
+      } else if (e.action === "delete") {
+        setCases((prevCases) =>
+          prevCases.filter((mycase) => mycase.id !== e.record.id)
+        );
+      }
+    });
   }, []);
   // Example coordinates for two places in Harare
   // const places = [
@@ -83,7 +83,6 @@ function MapPage() {
   //     }),
   //     'top-left'
   // );
-  const [selectedCase, setSelectedCase] = useState(null);
 
   return (
     <>
@@ -108,57 +107,72 @@ function MapPage() {
           cases.map((mycase, index) => (
             <Marker
               children={
-                <div>
-                  {(mycase?.status != "Cancelled" || mycase?.status != "Resolved") && (
+                <>
+                  {(mycase?.status != "Cancelled" ||
+                    mycase?.status != "Resolved") && (
                     <div className="flex flex-col justify-center items-center">
                       <div className="relative flex items-center justify-center">
-                        {mycase?.title.toString().toLowerCase().includes('sos') && (
-                            <div className="bg-white border-2 border-white rounded-full flex items-center justify-center">
-                                                        <span className="absolute w-10 h-10 rounded-full bg-red-500 opacity-75 animate-ping"></span>
+                        {mycase?.title
+                          .toString()
+                          .toLowerCase()
+                          .includes("sos") && (
+                          <div className="bg-white border-2 border-white rounded-full flex items-center justify-center">
+                            <span className="absolute w-10 h-10 rounded-full bg-red-500 opacity-75 animate-ping"></span>
 
-                                <Image
-                            src="/assets/images/sos.png"
-                            alt="marker"
-                            width={30}
-                            height={30}
-                            className="relative z-10"
-                          />
-                            </div>
-                            
+                            <Image
+                              src="/assets/images/sos.png"
+                              alt="marker"
+                              width={30}
+                              height={30}
+                              className="relative z-10"
+                            />
+                          </div>
                         )}
-                        {(mycase?.title.toString().toLowerCase().includes('accident') || mycase?.title.toString().toLowerCase().includes('traffic')) && (
-                            <div className="bg-white border-2 border-blue-600 rounded-full flex items-center justify-center">
-                                                                                        {/* <span className="absolute w-10 h-10 rounded-full bg-blue-500 opacity-75 animate-ping"></span> */}
+                        {(mycase?.title
+                          .toString()
+                          .toLowerCase()
+                          .includes("accident") ||
+                          mycase?.title
+                            .toString()
+                            .toLowerCase()
+                            .includes("traffic")) && (
+                          <div className="bg-white border-2 border-blue-600 rounded-full flex items-center justify-center">
+                            {/* <span className="absolute w-10 h-10 rounded-full bg-blue-500 opacity-75 animate-ping"></span> */}
 
-  <Image
-                            src="/assets/images/collision.png"
-                            alt="marker"
-                            width={30}
-                            height={30}
-                            className="relative z-10"
-                          />
-                            </div>
-                        
+                            <Image
+                              src="/assets/images/collision.png"
+                              alt="marker"
+                              width={30}
+                              height={30}
+                              className="relative z-10"
+                            />
+                          </div>
                         )}
-                         {(mycase?.title.toString().toLowerCase().includes('crime') || mycase?.title.toString().toLowerCase().includes('robbery')) && (
-                            <div className="bg-white border-2 border-orange-600 rounded-full flex items-center justify-center">
-                                                                                        {/* <span className="absolute w-10 h-10 rounded-full bg-orange-500 opacity-75 animate-ping"></span> */}
+                        {(mycase?.title
+                          .toString()
+                          .toLowerCase()
+                          .includes("crime") ||
+                          mycase?.title
+                            .toString()
+                            .toLowerCase()
+                            .includes("robbery")) && (
+                          <div className="bg-white border-2 border-orange-600 rounded-full flex items-center justify-center">
+                            {/* <span className="absolute w-10 h-10 rounded-full bg-orange-500 opacity-75 animate-ping"></span> */}
 
-  <Image
-                            src="/assets/images/robbery.png"
-                            alt="marker"
-                            width={30}
-                            height={30}
-                            className="relative z-10"
-                          />
-                            </div>
-                        
+                            <Image
+                              src="/assets/images/robbery.png"
+                              alt="marker"
+                              width={30}
+                              height={30}
+                              className="relative z-10"
+                            />
+                          </div>
                         )}
                       </div>
                       <p className="text-xs">{mycase?.title}</p>
                     </div>
                   )}
-                </div>
+                </>
               }
               key={index}
               longitude={mycase?.longitude}
@@ -188,7 +202,10 @@ function MapPage() {
             <div className="flex flex-col justify-between gap-y-1 items-center">
               <p>{selectedCase?.title}</p>
               <Badge variant="secondary">{selectedCase?.status}</Badge>
-              <TrackCase recordId={selectedCase.id} refreshParent={fetchCases} />
+              <TrackCase
+                recordId={selectedCase.id}
+                refreshParent={fetchCases}
+              />
               <Link
                 target="_blank"
                 rel="noopener noreferrer"
